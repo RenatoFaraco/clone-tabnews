166f11bddb444a3ce856535e9148571521c4cd7c
Renato Luiz Faraco Filho <renato.faraco.f@gmail.com>
2024-12-17 19:59:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Carina, eu te amo. Se voce me ama, dá uma risadinha!</h1>
+    return <h1>Carina, eu te amo. Se voce me ama, dá uma risadinha! :)</h1>
 }
 
 export default Home;
