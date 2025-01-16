4397acb4409341598f1adb7dcfa120bc954d93ad
Renato Luiz Faraco Filho <renato.faraco.f@gmail.com>
2025-01-16 22:17:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar e aprender programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Primeiramente... bom dia a todos :/</h1>
+  return <h1>Projeto em construção</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
