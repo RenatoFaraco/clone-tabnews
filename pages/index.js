2e6170263a92444c7a41526e2ec568a144953344
Renato Luiz Faraco Filho <renato.faraco.f@gmail.com>
2025-01-16 22:19:04 +0000

    Altera index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Projeto em construção</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
