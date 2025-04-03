fec49a6a39fbf34b7fdf3946e5a1e488843980dc
RenatoFaraco <renato.faraco.f@gmail.com>
2025-04-02 23:35:12 -0300

    make   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,12 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+  });
   await client.connect();
 
   try {
@@ -15,6 +21,7 @@ async function query(queryObject) {
     return result;  
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }  
