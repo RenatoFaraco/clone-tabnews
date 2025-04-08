4292f953ac5bc6b1b511647d15da241f16bd163b
RenatoFaraco <renato.faraco.f@gmail.com>
2025-04-08 03:08:28 -0300

    add `post.test.js` and migrations folders

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,34 @@
+import migrationRunner from 'node-pg-migrate'
+import {join} from "node:path"
+
+export default async function migrations(req, res) {
+  if (req.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations", 
+      noLock: true, //REMOVER ESSA SOLUÇÃO POSTERIORMENTE
+    });
+    return res.status(200).json(migrations);
+  }
+
+  if (req.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations", 
+      noLock: true, //REMOVER ESSA SOLUÇÃO POSTERIORMENTE
+    });
+    return res.status(200).json(migrations);
+  }
+
+  return res.status(405).end();
+
+}
+
