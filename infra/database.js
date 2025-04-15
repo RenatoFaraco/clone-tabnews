c541d02a55e50a00d683ec36a1bf6a4da0326ed2
RenatoFaraco <renato.faraco.f@gmail.com>
2025-04-14 21:35:46 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,10 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(), 
-  });
   
-
-  try { 
-    await client.connect();  
+  let client;
+  try {
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;  
   } catch (error) {
@@ -23,8 +15,23 @@ async function query(queryObject) {
   }  
 }
 
-export default {
-  query: query,
+async function getNewClient (){
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(), 
+  });
+
+  await client.connect();
+  return client;
+}
+
+export default {  
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
