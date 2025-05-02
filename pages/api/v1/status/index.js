2a9dd093bd6e8a41b41eb168e8fcd5240daf41e0
RenatoFaraco <renato.faraco.f@gmail.com>
2025-05-02 17:28:10 -0300

    fix `prettier` style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,18 +3,22 @@ import database from "infra/database.js";
 async function status(req, res) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult  = await database.query("SHOW server_version;");
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionVelue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB; 
+  const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
-    text:"SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-  const databaseOpenedConnectionsValue =  databaseOpenedConnectionsResult.rows[0].count;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
     updated_At: updatedAt,
