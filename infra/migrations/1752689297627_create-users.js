3b64bd7fc6f4e43994743e31921c9d576f1bcde9
RenatoFaraco <renato.faraco.f@gmail.com>
2025-07-17 15:29:56 -0300

    refactor: run `prettier:lint:fix`

--- infra/migrations/1752689297627_create-users.js
+++ infra/migrations/1752689297627_create-users.js
@@ -20,25 +20,25 @@ exports.up = (pgm) => {
       unique: true,
     },
 
-        // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
-        password: { 
-            type: "varchar(60)",
-            notNull: true,
-        },
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
 
-        // Why timestamptz? https://justatheory.com/2012/04/postgres-use-timestamptz/
-        created_at: {
-            type: "timestamptz",
-            notNull: true,
-            default: pgm.func("timezone('utc', now())"),
-        },
+    // Why timestamptz? https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
 
-        updated_at: {
-            type: "timestamptz",
-            notNull: true,
-            default: pgm.func("timezone('utc', now())"),
-        },
-    });
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
 };
 
 exports.down = false;
