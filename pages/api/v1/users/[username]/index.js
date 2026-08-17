81096cba8f0d681c4bc12c9a1915344d621c36da
Renato Faraco <renato.faraco.f@gmail.com>
2026-08-17 17:24:21 -0300

    feat: apply `authorization.filterOutput()` to all endpoints

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -13,9 +13,17 @@ router.patch(controller.canRequest("update:user"), patchHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
+  const userTryingToGet = req.context.user;
   const username = req.query.username;
   const userFound = await user.findOneByUsername(username);
-  return res.status(200).json(userFound);
+
+  const secureOutputValues = authorization.filterOutput(
+    userTryingToGet,
+    "read:user",
+    userFound,
+  );
+
+  return res.status(200).json(secureOutputValues);
 }
 
 async function patchHandler(req, res) {
@@ -33,6 +41,13 @@ async function patchHandler(req, res) {
     });
   }
 
-  const updateUser = await user.update(username, userInputValues);
-  return res.status(200).json(updateUser);
+  const updatedUser = await user.update(username, userInputValues);
+
+  const secureOutputValues = authorization.filterOutput(
+    userTryingToPatch,
+    "read:user",
+    updatedUser,
+  );
+
+  return res.status(200).json(secureOutputValues);
 }
